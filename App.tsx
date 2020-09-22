b8c79302d315cf20d0243d7a66b46e589c02fa83
itsgracian <gracian2020@gmail.com>
2020-09-22 18:16:06 +0200

    setup UI Kitten

--- App.tsx
+++ App.tsx
@@ -3,13 +3,18 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Routes } from './src/utils';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Button } from '@ui-kitten/components';
+import { default as theme} from './theme.json';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+      <Button>Welcome</Button>
+    </ApplicationProvider>
   );
 };
+
 export default App;
